f1080343b4fcbd038ecf4be413ff71fd619ffc9a
Nicholas Montanino <montanino@utexas.edu>
2018-03-15 20:03:16 -0400

    Set up connection from front-end to back-end

---
+++ client/src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tasks">
+    This page will contain all the tasks.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tasks',
+  data () {
+    return {}
+  }
+}
+</script>
